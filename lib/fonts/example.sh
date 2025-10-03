cd620aed081393f30c02cb4ff04333d35fb26b20
Gabriel Ramos <whosramoss@gmail.com>
2025-10-02 23:51:26 -0300

    chore: add fonts functions

---
+++ lib/fonts/example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source "lib/fonts/main.sh"
+
+sdx_show_script_title "AaBbCcDdEeFfGg"
+sdx_show_script_title "HhIJKkLlMmNnOoPpQqRr"
+sdx_show_script_title "SsTtUuVvWwXxYyZz"
+sdx_show_script_title "shelldx"
